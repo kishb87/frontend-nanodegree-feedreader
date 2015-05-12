e1aea4597f0ff8728fad092ea58963bd3f64fb68
kishb87 <kish.bplm@gmail.com>
2015-05-12 15:42:19 -0700

    Update readme

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -99,12 +99,6 @@ $(function() {
             done();
         });
 
-    
-
-    
-
-    /* TODO: Write a new test suite named "New Feed Selection"
-
         /* Ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
